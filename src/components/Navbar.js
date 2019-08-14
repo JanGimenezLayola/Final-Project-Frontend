a95c79e1722efb7d5908034408cfdfb818ac20f0
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-08-14 17:31:29 +0200

    Add elements to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,38 @@ import withAuth from './withAuth';
 
 
 class Navbar extends Component {
-  render() {  
+
+  state = {
+    menu: false
+  }
+
+  handleClick = () => {
+    if(this.state.menu){
+      this.setState({
+        menu: false
+      })
+    }else{
+      this.setState({
+        menu: true
+      })
+    }
+    
+  }
+
+  render() {   
+    const classMenu = this.state.menu ? 'navbar-opened' : 'navbar-closed';
     return (
-      <div>
+      <>
         {this.props.isLoggedIn ? (
-          <nav>
-            <p>username: {this.props.user.username}</p>
-            <button onClick={this.props.logout}>Logout</button>
+          <nav  className={classMenu} onClick={this.handleClick}>
+            <img src={this.props.user.image} alt="hola"/>
+            <section>
+              <h1>Welcome {this.props.user.email}</h1>
+              <button onClick={this.props.logout}>Logout</button>
+            </section>
           </nav>
         ) : null }   
-      </div>
+      </>
     )
   }
 }
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react'
 import withAuth from '../components/withAuth';
 
 class Dashboard extends Component {
-  render() {
-    
+  render() { 
     return (
       <div>
-        <h1>Welcome {this.props.user.username}</h1>
+        
       </div>
     )
   }
