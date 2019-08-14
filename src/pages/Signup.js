0666c063ea06d9f33ddc01b01ca6c3e77e96bae1
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-08-14 12:36:00 +0200

    add Formik and add errors feedback

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import { withFormik, Form, Field } from 'formik';
+
+import * as Yup from 'yup'
+
 import withAuth from '../components/withAuth';
 
 
@@ -11,44 +15,41 @@ class Signup extends Component {
     passwordRepeat: '',
   };
 
-  handleFormSubmit = (event) => {
-    event.preventDefault();
-    const email = this.state.email;
-    const password = this.state.password;
-    const passwordRepeat = this.state.passwordRepeat;
-    if(password === passwordRepeat) {
-      console.log(email, password)
-      this.props.signup({ email, password})
-      .then( (user) => {
-          console.log('Estic al then')
-          this.setState({
-              email: '',
-              password: '',
-              passwordRepeat: '',
-          })
-        })
-      .catch( error => console.log(error) )
-    } else {
-      console.log('Maaaaaaal')
-    }
-  } 
-
-  handleChange = (event) => {  
-    const {name, value} = event.target;
-    this.setState({[name]: value});
-  };
+  // handleFormSubmit = (event) => {
+  //   event.preventDefault();
+  //   const email = this.state.email;
+  //   const password = this.state.password;
+  //   const passwordRepeat = this.state.passwordRepeat;
+  //   if(password === passwordRepeat) {
+  //     console.log(email, password)
+  //     this.props.signup({ email, password})
+  //     .then( (user) => {
+  //         console.log('Estic al then')
+  //         this.setState({
+  //             email: '',
+  //             password: '',
+  //             passwordRepeat: '',
+  //         })
+  //       })
+  //     .catch( error => console.log(error) )
+  //   } else {
+  //     console.log('Maaaaaaal')
+  //   }
+  // } 
 
   render() {
-    const { email, password , passwordRepeat} = this.state;
-    console.log(passwordRepeat)
     return (
       <section className="main-splash">
-        <form onSubmit={this.handleFormSubmit}>
-          <input id='email' type='email' name='email' placeholder='email' value={email} onChange={this.handleChange} autoComplete="email"/>
-          <input id='password' type='password' name='password' placeholder='password' value={password} onChange={this.handleChange} autoComplete="new-password" />
-          <input id='passwordRepeat' type='password' name='passwordRepeat' placeholder=' repeat password' value={passwordRepeat} onChange={this.handleChange} autoComplete="new-password" />
-          <input className='submit-button' type='submit' value='Signup' />
-        </form>
+        <Form autoComplete="off">
+          <Field type='email' name='email' placeholder='email' />
+          {this.props.touched.email && this.props.errors.email && <p>{this.props.errors.email}</p>}
+          <Field type='password'name='password' placeholder='password' />
+          {this.props.touched.password && this.props.errors.password && <p>{this.props.errors.password}</p>}
+          <Field type='password' name='passwordRepeat' placeholder='repeat password' />
+          {this.props.touched.passwordRepeat && this.props.errors.passwordRepeat && <p>{this.props.errors.passwordRepeat}</p>}
+          {this.props.errors.passwordMatch && <p>{this.props.errors.passwordMatch}</p>}
+          <button className='submit-button' type='submit'>Signup</button>
+        </Form>
 
         <p>Already have account? 
           <Link to={'/'}> Login</Link>
@@ -59,4 +60,44 @@ class Signup extends Component {
   }
 }
 
-export default withAuth(Signup);
+export default withAuth(withFormik({
+  mapPropsToValues({ email, password, passwordRepeat }) {
+    return ({
+      email: email || '',
+      password: password || '',
+      passwordRepeat: passwordRepeat || '',
+    })		
+  },
+  handleSubmit(values, bag) {
+    console.log(values)
+
+    const email = values.email;
+    const password = values.password;
+    const passwordRepeat = values.passwordRepeat;
+    if(password !== passwordRepeat) {
+      bag.setErrors({
+        passwordMatch: 'passwords doesnt match'
+      })
+    } else {
+      bag.props.signup({ email, password})
+      .then( (user) => {
+        this.setState({
+            email: '',
+            password: '',
+            passwordRepeat: '',
+        })
+      })
+    .catch( error => console.log(error) )
+    }
+  },
+  validationSchema: Yup.object().shape({
+    email: Yup.string()
+      .email('wrong email')
+      .required('email is required'),
+    password: Yup.string()
+      .required('password is required')
+      .min(8),
+    passwordRepeat: Yup.string()
+      .required('password is required')
+    })
+  })(Signup));
