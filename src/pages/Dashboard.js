85e9a679c5dd29eb52735e3a75e0dab6fcca0914
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-08-18 19:38:49 +0200

    change structure dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import withAuth from '../components/withAuth';
 
 
-// import Countdown from '../components/Countdown.jsx'
+import Countdown from '../components/Countdown.jsx'
+import Popup from '../components/PopupActivities.jsx'
 
 import tripsService from '../services/trips-service';
 
@@ -12,36 +13,78 @@ class Dashboard extends Component {
     country: '',
     date: '',
     name: '',
+    showPopup: false,
+    activities: {
+      name: '',
+      date: '',
+      id: ''
+    }
   }
 
   componentDidMount() {
     const id = this.props.match.params.id
     tripsService.view(id)
-    .then( (response) => {
+    .then( (response) => {      
       this.setState({
         name: response[0].name,
         country: response[0].country,
         date: response[0].date
-      })     
+      })   
+      tripsService.activitiesList(id)
+      .then( (response) => {
+        console.log(response);
+        this.setState({
+          activities: response
+        })
+      })
+        .catch( error => console.log(error) )
+    })
+    .catch(error => console.log(error) );
+  };
+
+
+
+
+  togglePopup() {  
+    this.setState({  
+         showPopup: !this.state.showPopup  
     })
   }
 
   render() { 
-  
-    console.log(this.props)
-    // const tripId = ;
-    // console.log(tripId);
-    // tripsArray.map((trip) => {
-    //   if(trip.props.user)
-    // })
+    console.log(this.state);
+    
     return (
-      <>
-        <h1>{this.state.name}</h1>
-        <article>
-          {/* <Countdown /> */}
-        </article>
+      <section className='dashboard-container'>
+        <h1 className='trip-name'>{this.state.name}</h1>
+        {this.state.showPopup ?  
+          <Popup  
+            text='Add a new activity'  
+            closePopup={this.togglePopup.bind(this)}  
+            props={this.props}
+          /> 
+          : null } <>
+          <article className='card'>
+          <Countdown tripDate={this.state.date} />
+          </article>
+          <article className='card'>
+            <button onClick={this.togglePopup.bind(this)}>Add new activity</button> 
+            {this.state.activities.activities ? this.state.activities.activities.map((activity) => {
+              return (
+                <article>
+                  <h3>{activity.name}</h3>
+                </article>
+              )
+            }): null }
+          </article>         
+          <article className='card'>
+          </article>
+          <article className='card'>
+            
+          </article>
+          </>   
         
-      </>
+      </section>
     )
   }
 }
