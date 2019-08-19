5538f3c7373b276fc66f149aee691b8b2ba97d16
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-08-19 20:31:39 +0200

    work popus forms

--- src/components/PopupUpdate.jsx
+++ src/components/PopupUpdate.jsx
@@ -2,16 +2,39 @@ import React from 'react';
 import tripsService from '../services/trips-service';
 
 import { Redirect } from 'react-router-dom';
+import withAuth from './../components/withAuth';
 
 import { withFormik, Form, Field } from 'formik';
 // import * as Yup from 'yup'
 
-class Popup extends React.Component {  
+class PopupUpdate extends React.Component {  
 
   state={
     name: '',
     date: '',
-    redirect: false
+    country: '',
+    activities: [],
+    redirect: false,
+    id: ''
+  }
+
+  componentDidMount() {
+    // console.log(this.props);
+    console.log(this.props.props.match.params.id);
+    
+    const id = this.props.props.match.params.id
+    tripsService.activitiesList (id)
+    .then( (response) => {    
+      console.log(response);
+       
+      this.setState({
+        name: response.name,
+        country: response.country,
+        date: response.date,
+        activities: response.activities
+      })   
+    })
+    .catch(error => console.log(error) )
   }
 
   render() {  
@@ -25,14 +48,14 @@ if(this.state.redirect){
         <button onClick={this.props.closePopup}>X</button>  
         <Form className='create-activity' autoComplete="off" redirect={this.state.redirect}>
           <h1>{this.props.text}</h1>
+          {console.log(this.props)}
           <section>
-            <Field hidden type='text' name='redirect'/>
             <label htmlFor="name">Name</label>
-            <Field type='name' name='name' placeholder='Visit Machu Pichu' />
+            <Field type='name' name='name' value={this.state.name} />
           </section>
           <section>
            <label htmlFor="date">Description</label>
-           <Field className='date' type='datetime-local' name='date' />
+           <Field className='date' type='datetime-local' name='date'  value={this.props.values.date}/>
           </section>
           {/* <section>
             <label htmlFor="Tickets">Description</label>
@@ -42,29 +65,29 @@ if(this.state.redirect){
             <label htmlFor="price">Description</label>
             <Field type='number' name='price' placeholder='75' />
           </section> */}
-          <button onClick={this.handleSubmit}>Submit</button>
+          <button onClick={() => this.handleSubmit(this.props)}>Submit</button>
         </Form> 
-      </div>  
+      </div> 
     </div>  
     );  
   } 
 }  
 
-export default withFormik({
+export default withAuth(withFormik({
   mapPropsToValues({ name, date }) {    
-    
     return ({
       name: name || '',
       date: date || '',
     })
   },
-  handleSubmit(values, props)  {
-    const id = props.props.props.match.params.id
-    const name = values.name;
-    const date = values.date;    
+  handleSubmit(props)  {
+    console.log(props.match.params.id);
+    const id = this.props.match.params.id
+    const name = props.values.name;
+    const date = props.values.date;    
     tripsService.addActivity({ id, name, date })
     .then (response => {
     })
     .catch( error => console.log(error) )
   },
-})(Popup);
+})(PopupUpdate));
