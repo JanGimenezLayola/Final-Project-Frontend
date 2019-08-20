bf6e5ad46475eb0b2a74af5fc5252dedbb56da9e
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-08-20 16:12:14 +0200

    create user services and add getUsers route

---
+++ src/services/users-services.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+
+
+class UsersService {
+  constructor () {
+    this.users = axios.create({
+      baseURL: `${process.env.REACT_APP_BACKEND_DOMAIN}/usersApi`,
+      withCredentials: true
+    });
+  }
+
+  getUsers (id) {
+    return this.users.get(`/getUsers`)
+    .then(response => response.data)
+  }  
+  
+}
+
+const usersService = new UsersService();
+
+export default usersService;
