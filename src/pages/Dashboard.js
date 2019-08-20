c27e85dfff2a99b52a24f29996397cdb48275930
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-08-20 16:11:27 +0200

    delete debuger consolelogs and comments

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,6 +5,7 @@ import withAuth from '../components/withAuth';
 import Countdown from '../components/Countdown.jsx'
 import Popup from '../components/PopupActivities.jsx'
 import PopupUpdate from '../components/PopupUpdate.jsx'
+import SearchBar from '../components/SearchBar.jsx'
 
 import tripsService from '../services/trips-service';
 import moment from 'moment';
@@ -24,7 +25,16 @@ class Dashboard extends Component {
     const id = this.props.match.params.id
     tripsService.activitiesList (id)
     .then( (response) => {    
-      console.log(response);
+      console.log('resposne -->', response.activities);
+
+      const order = response.activities.sort(function(a, b) {
+        console.log('soy a', a.date);
+        
+        
+    //     b.date = new Date(b.dateModified);
+      return a.date>b.date ? 1 : a.date<b.date ? -1 : 0
+    });
+       console.log('order --> ',order);
        
       this.setState({
         name: response.name,
@@ -45,7 +55,9 @@ class Dashboard extends Component {
     })
   }
 
- 
+ handleSearchBarChange(props) {
+
+ }
 
   togglePopup() {  
     this.setState({  
@@ -53,9 +65,11 @@ class Dashboard extends Component {
     })
   }
   togglePopupUpdate(id) {  
+    console.log(id);
+    
     this.setState({  
          updatePopup: !this.state.updatePopup,
-         activitiId: id
+         activityId: id
     })
   }
 
@@ -77,6 +91,7 @@ class Dashboard extends Component {
           <PopupUpdate  
             text='Update activity'  
             closePopup={this.togglePopupUpdate.bind(this)}  
+            activityId={this.state.activityId}
             props={this.props}
           /> 
           : null 
@@ -86,20 +101,31 @@ class Dashboard extends Component {
           {/* <Countdown tripdate={moment(this.state.date).format('MMMM Do YYYY, h:mm a')} /> */}
           <Countdown timeTillDate={moment(this.state.date).format('MMMM DD YYYY, h:mm a')} timeFormat="MM DD YYYY, h:mm a" />
           </article>
-          <article className='card'>
+          <article className='card card-activities'>
             <button onClick={this.togglePopup.bind(this)}>Add new activity</button> 
             {/* {console.log(this.state.activities) } */}
             {this.state.activities.length > 0 ? this.state.activities.map((activity) => {
               return (
-                <article>
-                  <h3>{activity.name}</h3>
-                  <button onClick={() => {this.handleDelete(activity._id)}}>X</button>
-                  <button onClick={this.togglePopupUpdate.bind(this, activity._id)}>Edit</button> 
+                <article className='activities'>
+                  <section className='date-hour'>
+                    <h3>{moment(activity.date).format('H:mm')}</h3>
+                    <section>
+                      <h3>{moment(activity.date).format('DD')}</h3>
+                      <h3>/</h3>
+                      <h3>{moment(activity.date).format('MMM')}</h3>
+                    </section>
+                  </section>
+                  <h3 >{activity.name}</h3>  
+                  <section>
+                    <button onClick={() => {this.handleDelete(activity._id)}}>X</button>
+                    <button onClick={this.togglePopupUpdate.bind(this, activity._id)}>Edit</button> 
+                  </section>
                 </article>
               )
             }): null }
           </article>         
           <article className='card'>
+            <SearchBar />
           </article>
           <article className='card'>
             
